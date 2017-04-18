1621a586e190f3b50bfd05b62ee9455a2c63d746
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-18 11:21:18 +0200

    Black jack exercise started, color counter created

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -1,3 +1,5 @@
+import random
+
 # Create a Card class, that has a color and a value
 # Create a constructor for setting those values
 # Card should be represented as string in this format:
@@ -10,13 +12,44 @@
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-deck = Deck(12)
-print(deck)
-# Should print out:
-# 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-top_card = deck.draw()
-print(top_card)
-print(deck)
+# deck = Deck(12)
+# print(deck)
+# # Should print out:
+# # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+# top_card = deck.draw()
+# print(top_card)
+# print(deck)
 # Should print out:
 # Queen Spades
 # 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
+
+class Card():
+
+    def __init__(self):
+        self.color = ["Clubs", "Diamonds", "Hearts", "Spades"]
+        self.value = ["2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"]
+
+class Deck():
+
+    def __init__(self, number):
+        self.number = number
+        self.card = Card()
+        self.list_of_cards = []
+        self.number_of_colors = ""
+        self.color_counter()
+
+    def color_counter(self):
+        number_of_colors = ""
+        for i in range(self.number//4):
+            for color in self.card.color:
+                self.list_of_cards.append(random.choice(self.card.value))
+                self.list_of_cards.append(color)
+        for i in range(self.number%4):
+            self.list_of_cards.append(random.choice(self.card.value))
+            self.list_of_cards.append( self.card.color[(i-1)+1])
+        self.number_of_colors += str(self.number) + " cards - " + str(self.list_of_cards.count("Clubs")) + " Clubs, " + str(self.list_of_cards.count("Diamonds")) + " Diamonds, " + str(self.list_of_cards.count("Hearts")) + " Hearts, " + str(self.list_of_cards.count("Spades")) + " Spades"
+        print(self.list_of_cards)
+        print(self.number_of_colors)
+        return self.number_of_colors
+
+deck = Deck(12)
