8b6ad117ac555bdc57e24ee5e5f3c8386987bbf0
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-18 10:14:57 +0200

    Copy and paste exercise finished, works properly

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,28 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+
+class Copy():
+
+    def __init__(self, source = "[source]", destination = "[destination]"):
+        self.source = source
+        self.destination = destination
+        if self.source == "[source]" and self.destination == "[destination]":
+            print("copy " + self.source + self.destination)
+        elif self.source != "[source]" and self.destination == "[destination]":
+            print("No destination provided")
+        else:
+            self.copy_paste()
+
+    def copy_paste(self):
+        try:
+            source_file = open(self.source, 'r')
+            source = source_file.read()
+            destination_file = open(self.destination, 'w')
+            destination_file.write(source)
+            source_file.close
+            destination_file.close
+        except FileNotFoundError:
+            return print("That is not a file")
+
+copy = Copy('copy_this.txt', 'paste_here.txt')
